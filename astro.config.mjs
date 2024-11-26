ba8ac1ff6183f89542c80e6266ce8f1f260e69a1
Ender <ndxendernight@gmail.com>
2024-11-26 21:19:16 +0100

    feat(base): changed root

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static'
+    site: 'https://endernight.github.io',
+    base: '/EnderNight',
 });
